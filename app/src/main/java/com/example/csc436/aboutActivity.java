3dcf49d773a6bbf051b0fe05bd037c270b132a54
Francois Aouad <86322208+FrancoisAouad@users.noreply.github.com>
2022-04-21 11:47:54 +0300

    CSC436 Advanced Programming Project //April 2022

--- app/src/main/java/com/example/csc436/aboutActivity.java
+++ app/src/main/java/com/example/csc436/aboutActivity.java
@@ -3,31 +3,63 @@ package com.example.csc436;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class aboutActivity extends AppCompatActivity {
+
+    Button about_back;
+    ImageView about_insta, about_twitter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
+        setContentView(R.layout.activity_new_layout);
+
+      about_back=findViewById(R.id.backabout);
+       about_insta=findViewById(R.id.insta);
+       about_twitter=findViewById(R.id.twitter);
+
+
+
+        about_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(aboutActivity.this, mainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        about_insta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri insta = Uri.parse("https://instagram.com/green.org.leb?igshid=YmMyMTA2M2Y=");
+                Intent webIntent2 = new Intent(Intent.ACTION_VIEW, insta);
+                startActivity(webIntent2);
+            }
+        });
+
+        about_twitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Uri twitter = Uri.parse("https://twitter.com/greenorgleb?s=21&t=cdbbP9rMhecAwcX8fowM-w");
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, twitter);
+                startActivity(webIntent);
+
+            }
+        });
+
+
 
 
 
     }
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
 
-
-        switch(item.getItemId()){
-            case android.R.id.home:
-                finish();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-
-        }
-
-    }
 }
--- app/src/main/java/com/example/csc436/writeActivity.java
+++ app/src/main/java/com/example/csc436/writeActivity.java
@@ -1,6 +1,7 @@
 package com.example.csc436;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -14,6 +15,8 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
@@ -29,14 +32,14 @@ import java.util.HashMap;
 
 public class writeActivity extends AppCompatActivity {
 
-    Toolbar tool_bar;
+    //  Toolbar tool_bar;
     Button write_button, write_back;
     EditText write_text;
     DatabaseReference userRef, postRef;
     FirebaseAuth mauth;
     String savecurrentdate, savecurrenttime;
 
-  //  String current_user = mauth.getCurrentUser().getUid();
+    //  String current_user = mauth.getCurrentUser().getUid();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,12 +47,10 @@ public class writeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_write);
 
 
-
-
-        write_button=findViewById(R.id.writepostbutton);
-        write_text=findViewById(R.id.writeposttext);
+        write_button = findViewById(R.id.writepostbutton);
+        write_text = findViewById(R.id.writeposttext);
         mauth = FirebaseAuth.getInstance();
-        write_back=findViewById(R.id.writepostback);
+        write_back = findViewById(R.id.writepostback);
 
         String current_user = mauth.getCurrentUser().getUid();
 
@@ -69,67 +70,97 @@ public class writeActivity extends AppCompatActivity {
                 savecurrenttime = currentime.format(time.getTime());
 
                 postRef = FirebaseDatabase.getInstance().getReference().child("posts").child(current_user);
-               userRef = FirebaseDatabase.getInstance().getReference().child("users").child(current_user);
-               // String current_user = mauth.getCurrentUser().getUid();
+                userRef = FirebaseDatabase.getInstance().getReference().child("users").child(current_user);
+                // String current_user = mauth.getCurrentUser().getUid();
 
-                userRef.child(current_user).addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        if (snapshot.exists()) {
-                             String full_name = snapshot.child("fullname").getValue().toString();
-                             String user_name = snapshot.child("username").getValue().toString();
-                             String pfp = snapshot.child("profileimage").getValue().toString();
+userRef.addValueEventListener(new ValueEventListener() {
+    @Override
+    public void onDataChange(@NonNull  DataSnapshot snapshot) {
+        if(snapshot.exists()){
 
-                            HashMap newposts = new HashMap();
-
-                            newposts.put("id", current_user);
-                            //newposts.put("fullname", full_name);
-                            //newposts.put("username", user_name);
-                            newposts.put("date", savecurrentdate);
-                            newposts.put("time", savecurrenttime);
-                            newposts.put("text", post);
-                           // newposts.put("profileimage", pfp);
-
-                            postRef.updateChildren(newposts).addOnCompleteListener(new OnCompleteListener() {
-                                @Override
-                                public void onComplete(@NonNull Task task) {
-                                    if (task.isSuccessful()) {
-                                        Intent intent = new Intent(writeActivity.this, mainActivity.class);
-                                        startActivity(intent);
-                                        finish();
-
-                                        Toast.makeText(writeActivity.this, "Post Sent", Toast.LENGTH_SHORT).show();
-
-                                    } else {
-                                        Toast.makeText(writeActivity.this, task.getException().toString(), Toast.LENGTH_SHORT).show();
-                                        finish();
-                                    }
-                                }
-                            });
-                        }
-
-                    }
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-                        Log.d("TAG", error.getMessage()); //display errors
-                    }
-                });
+            String full_name = snapshot.child("fullname").getValue().toString();
+            String user_name = snapshot.child("username").getValue().toString();
+            //String pfp = snapshot.child("profileimage").getValue().toString();
 
 
-                write_back.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(writeActivity.this, mainActivity.class);
-                        startActivity(intent);
-                        finish();
-                    }
-                });
+            HashMap newposts = new HashMap();
+
+            newposts.put("id", current_user);
+            newposts.put("fullname", full_name);
+            newposts.put("username", user_name);
+            newposts.put("date", savecurrentdate);
+            newposts.put("time", savecurrenttime);
+            newposts.put("text", post);
+            // newposts.put("profileimage", pfp);
+
+            postRef.child("newposts").push().setValue(newposts).addOnCompleteListener(task ->
+            {
 
 
-            }
+                if (task.isSuccessful()) {
+                    Toast.makeText(writeActivity.this, "Post sent", Toast.LENGTH_SHORT).show();
 
-        });
+
+                } else {
+
+                    Toast.makeText(writeActivity.this, "error", Toast.LENGTH_SHORT).show();
+                }
+
+            });
+
+
+        }
     }
+
+    @Override
+    public void onCancelled(@NonNull  DatabaseError error) {
+
+    }
+});
+
+
+           /*     HashMap newposts = new HashMap();
+
+                 newposts.put("id", current_user);
+                 newposts.put("fullname", full_name);
+                newposts.put("username", user_name);
+                newposts.put("date", savecurrentdate);
+                newposts.put("time", savecurrenttime);
+                newposts.put("text", post);
+                // newposts.put("profileimage", pfp);
+
+                postRef.child("newposts").push().setValue(newposts).addOnCompleteListener(task ->
+                {
+
+
+                    if (task.isSuccessful()) {
+                        Toast.makeText(writeActivity.this, "Post sent", Toast.LENGTH_SHORT).show();
+
+
+                    } else {
+
+                        Toast.makeText(writeActivity.this, "error", Toast.LENGTH_SHORT).show();
+                    }
+
+                });*/
+            }
+        });
+
+        write_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(writeActivity.this, mainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+    }
+
 }
+
+
+
+
 
